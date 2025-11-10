3dcf72d769b0e9de65ba485f20821acd07657a20
Artem K. <artem.kabatov22@gmail.com>
2025-11-10 22:33:24 +0300

    feat: add redirect from short url

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import secrets
 
-from fastapi import FastAPI, HTTPException, status, Depends
+from fastapi import FastAPI, HTTPException, status, Depends, Request
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 import validators
 
@@ -10,12 +11,18 @@ from .database import SessionLocal, engine
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+def raise_not_found(request: Request):
+    message = f"URL '{request.url}' doesn't exist."
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=message)
 
 
 def raise_bad_request(message: str) -> None:
@@ -44,7 +51,6 @@ async def read_root() -> dict:
         "status": "active"
     }
 
-
 @app.post("/url", response_model=schemas.URLInfo)
 def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -63,3 +69,19 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     db_url.admin_url = secret_key
 
     return db_url
+
+@app.get("/{url_key}")
+def forward_to_target_url(
+        url_key: str,
+        request: Request,
+        db: Session = Depends(get_db)
+):
+    db_url = (
+        db.query(models.URL)
+        .filter(models.URL.key == url_key, models.URL.is_active)
+        .first()
+    )
+    if db_url:
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
