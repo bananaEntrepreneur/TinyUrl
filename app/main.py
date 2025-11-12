72d788eb62ee5b77e6d9d013fc7fd50ce2d65680
Artem K. <artem.kabatov22@gmail.com>
2025-11-12 10:40:45 +0300

    feat: url info by secret key endpoint

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import secrets
-import validators
+from urllib import request
 
 from fastapi import FastAPI, HTTPException, status, Depends, Request
 from fastapi.responses import RedirectResponse
@@ -74,5 +74,16 @@ def forward_to_target_url(
     )
     if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
         return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
+
+@app.get("/admin/{secret_key}",
+         name="administration info",
+         response_model=schemas.URLInfo)
+def get_url_info(secret_key: str, db: Session = Depends(get_db)):
+    if db_url := crud.get_db_url_by_secret_key(db=db, secret_key=secret_key):
+        db_url.url = db_url.key
+        db_url.admin_url = db_url.secret_key
+        return db_url
     else:
         raise_not_found(request)
